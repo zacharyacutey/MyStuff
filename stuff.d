7840fe27ce1898b42bf79da5844108f1c998da0e
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-05-17 09:32:28 -0400

    Golfed even further

--- stuff.d
+++ stuff.d
@@ -1,4 +1,1 @@
-bool s(int[]a,int v){int[]t;int i,j;while(i!=a.length)t~=a[i++]%v;while(j!=a.length)if(t[j++]!=0)return 0;return 1;}
-int g(int[]a){int r=a[0];int i;while(i!=a.length){r=r>a[i]?a[i]:r;i++;}return r;}
-bool is_reduce(int[]a){int i=2;while(i<=a.g)if(s(a,i++))return 0;return 1;}
-void main(){}
+bool s(int[]a,int v){int[]t;int i,j;while(a.length-i)t~=a[i++]%v;while(a.length-j)if(t[j++])return 0;return 1;}int g(int[]a){int r=a[0],i;while(a.length-i){r=r>a[i]?a[i]:r;i++;}return r;}bool e(int[]a){int i=2;while(i<=a.g)if(s(a,i++))return 0;return 1;}void main(){}
