bd9d5ca5914973ff87eefeaeffe9d9512b3cc699
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-05-20 17:12:59 -0400

    Update FractionReduce.d

--- Golf/FractionReduce.d
+++ Golf/FractionReduce.d
@@ -1,13 +1,1 @@
-int[2] reduced_fraction(int[2] arg)
-{
-  int i = 2;
-  while(i <= arg[0])
-  {
-    while(arg[0] % i == [0,0])
-    {
-      arg[] /= i;
-    }
-    i++;
-  }
-  return arg;
-}
+int[2]reduced_fraction(int[2] arg){int i=2;while(i<=arg[0]){while(arg[0]%i==[0,0]){arg[]/=i;}i++;}return arg;}
