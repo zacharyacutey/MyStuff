de09d0621647d31657b62177137ed8af676d8903
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-05-24 09:50:32 -0400

    Update reduceratio_golf.d

--- reduceratio_golf.d
+++ reduceratio_golf.d
@@ -1,8 +1,6 @@
-import std.stdio;
-
-bool is_divisible(int[] arr,int arg)
+bool is_reduce(int[] arr,int arg)
 {
-  int i = 0;
+  int i;
   while(i!=arr.length)
   {
     if(arr[i]%arg!=0)
@@ -12,54 +10,4 @@ bool is_divisible(int[] arr,int arg)
     i++;
   }
   return true;
-
-}
-
-void divide(ref int[] arr,int arg)
-{
-  int i = 0;
-  while(i!=arr.length)
-  {
-    arr[i]/=arg;
-    i++;
-  }
-}
-
-int min(int[] arr)
-{
-  int r = arr[0];
-  int i = 0;
-  while(i!=arr.length)
-  {
-    if(arr[i]<r)
-    {
-      r=arr[i];
-    }
-    i++;
-  }
-  return r;
-}
-
-int[] reduced(int[] arg)
-{
-  int i = 2;
-  while(i<=min(arg))
-  {
-    if(is_divisible(arg,i))
-    {
-      divide(arg,i);
-    }
-    i++;
-  }
-  return arg;
-}
-
-void reduce(ref int[] arg)
-{
-  arg=reduced(arg);
-}
-
-void main()
-{
-  
 }
