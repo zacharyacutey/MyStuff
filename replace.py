e9ffc5a3216003f8b6583181adb41e3ff633d7bf
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-07-26 17:12:08 -0400

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,4 +1,3 @@
-#Rule Based Longest translation function, very useful.
 def replace(rules,string,default=''):
  _ = True
  lower=0
@@ -15,6 +14,25 @@ def replace(rules,string,default=''):
    else:
     upper -= 1
   if _:
-   result+=default
+   result += default
+   lower += 1
  return result
-
+def sreplace(rules,string):
+ _ = True
+ lower=0
+ result=''
+ while lower < len(string):
+  _ = True
+  upper = len(string)
+  while upper > lower:
+   if string[lower:upper] in rules:
+    result+=rules[string[lower:upper]]
+    lower += len(string[lower:upper])
+    upper = lower
+    _ = False
+   else:
+    upper -= 1
+  if _:
+   result += string[lower]
+   lower += 1
+ return result
