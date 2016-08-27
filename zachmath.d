a259fca23a9cf6dc39136b738d7e18575350d66a
Zacharý <zacharyacutey@users.noreply.github.com>
2016-08-27 09:56:00 -0400

    Update zachmath.d

--- zachmath.d
+++ zachmath.d
@@ -40,4 +40,54 @@ class Fraction
 		this.denominator = d;
 		this.simplify();
 	}
+	public int getNumerator()
+	{
+		return this.numerator;
+	}
+	public int getDenominator()
+	{
+		return this.denominator;
+	}
+	public Fraction opAdd(Fraction rhs)
+	{
+		return new Fraction(this.getNumerator()*rhs.getDenominator()+rhs.getNumerator()*this.getDenominator(),
+		this.getDenominator()*rhs.getDenominator());
+	}
+	public Fraction opNeg()
+	{
+		return new Fraction(-this.getNumerator(),this.getDenominator());
+	}
+	public Fraction opSub(Fraction rhs)
+	{
+		return this+(-rhs);
+	}
+	public Fraction opMul(Fraction rhs)
+	{
+		return new Fraction(this.getNumerator()*rhs.getNumerator(),this.getDenominator()*rhs.getDenominator());
+	}
+	public Fraction opDiv(Fraction rhs)
+	{
+		return this*(new Fraction(rhs.getDenominator(),rhs.getDenominator()));
+	}
+	public bool opEquals(Fraction rhs)
+	{
+		return this.getNumerator() == rhs.getNumerator() && this.getDenominator() == rhs.getDenominator();
+	}
+	public int opCmp(Fraction rhs)
+	{
+		int left = this.getNumerator() * rhs.getDenominator();
+		int right = this.getDenominator() * rhs.getNumerator();
+		if(left == right)
+		{
+			return 0;
+		}
+		else if(left < right)
+		{
+			return -1;
+		}
+		else
+		{
+			return 1;
+		}
+	}
 }
