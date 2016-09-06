8e7c7651b1b3f2c21d462a93d1283d741c1cb162
Zacharý <zacharyacutey@users.noreply.github.com>
2016-09-06 16:40:27 -0400

    This isn't TI-Basic, DUH!

--- AlgebraHomework/HomeworkA14.py
+++ AlgebraHomework/HomeworkA14.py
@@ -13,7 +13,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
-There's not a way to comment in TI-Basic I know of, so this line and above are NOT part of the source to be copied into the calculator
 """
 from sympy import *
 from functools import reduce
