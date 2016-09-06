cabe28b3e3fdafbe38f4e3245b3713672fbbf5db
Zacharý <zacharyacutey@users.noreply.github.com>
2016-09-06 16:30:01 -0400

    Update S_M_D_RE.py

--- AlgebraHomework/S_M_D_RE.py
+++ AlgebraHomework/S_M_D_RE.py
@@ -2,7 +2,8 @@ from sympy import *
 import sys
 sys.displayhook = pprint
 def hw_simplify(numerator,denominator):
- print("("+str(numerator)+")/("+str(denominator)+")")
- print("("+str(factor(numerator))+")/("+str(denominator)+")")
- print(str(factor(numerator)/factor(denominator)))
- print(solve(denominator))
+	print("("+str(numerator)+")/("+str(denominator)+")")
+	print("("+str(factor(numerator))+")/("+str(denominator)+")")
+	print(str(factor(numerator)/factor(denominator)))
+	for i in solve(denominator,x):
+		print("x != "+str(i))
