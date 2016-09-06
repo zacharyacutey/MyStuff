70928da7f42226de14d56054b4c5c47c35bd8e18
Zacharý <zacharyacutey@users.noreply.github.com>
2016-09-06 17:43:52 -0400

    Update

--- AlgebraHomework/S_M_D_RE.py
+++ AlgebraHomework/S_M_D_RE.py
@@ -6,5 +6,12 @@ def hw_simplify(numerator,denominator):
 	print("("+str(factor(numerator))+")/("+str(factor(denominator))+")")
 	print(str(factor(numerator)/factor(denominator)))
 	for i in solve(denominator,x):
-		if not (i in solve(numerator/denominator)):
-			print("x != "+str(i))
+		print("x != "+str(i))
+def hw_multiply(n1,d1,n2,d2):
+	print(str(n1)+"/"+str(d1)+"*"+str(n2)+"/"+str(d2))
+	numerator = n1 * n2
+	denominator = d1 * d2
+	print("("+str(factor(numerator))+")/("+str(factor(denominator))+")")
+	print(str(factor(numerator)/factor(denominator)))
+	for i in solve(denominator,x):
+		print("x != "+str(i))
