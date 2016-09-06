86e1760525230b84d8572ae00c0590857735bd42
Zacharý <zacharyacutey@users.noreply.github.com>
2016-09-06 16:24:39 -0400

    Create S_M_D_RE.py

---
+++ AlgebraHomework/S_M_D_RE.py
@@ -0,0 +1,8 @@
+from sympy import *
+import sys
+sys.displayhook = pprint
+def hw_simplify(numerator,denominator):
+ print(str(numerator)+"/("+str(denominator)+")")
+ print(str(factor(numerator))+"/("+str(denominator)+")")
+ print(str(factor(numerator)/factor(denominator)))
+ print(solve(denominator))
